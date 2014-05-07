73bd2a8b36b5d44dc4b3eab017a3578243949d1f
Martin Fischer <Arthur.Spooner@gmail.com>
2014-05-07 22:38:47 +0200

    fixed missing props to parse datetime values failsafe when setting up the sqlite connection

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -2,27 +2,29 @@
 // http://flnkr.com/2011/06/using-sqlite-with-grails/
 
 dataSource {
-   dbCreate = ""
-   url = "jdbc:sqlite:./data/data.db"
-   logSql = true
-   dialect = "dialect.SQLiteDialect"
-   driverClassName = "org.sqlite.JDBC"
-   readOnly = "true"
-   dateStringFormat = "yyyy-MM-dd HH:mm:ss"
-   datePrecision = Calendar.SECOND
-   grails.dbconsole.enabled = true
+	pooled = true
+	dbCreate = ""
+	url = "jdbc:sqlite:./data/data.db"
+	logSql = false
+	dialect = "dialect.SQLiteDialect"
+	driverClassName = "org.sqlite.JDBC"
+	readOnly = "true"
+	grails.dbconsole.enabled = true
+	properties {
+		// see tomcat.apache.org/tomcat-7.0-doc/jdbc-pool.html#Configuring_JDBC_interceptors
+		connectionProperties = "date_precision=seconds;date_string_format=yyyy-MM-dd HH:mm:ss"
+	}
 }
+
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+	cache.use_second_level_cache = true
+	cache.use_query_cache = false
+	cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
+
 // environment specific settings
 environments {
-    development {
-    }
-    test {
-    }
-    production {
-    }
+	development { }
+	test { }
+	production { }
 }
