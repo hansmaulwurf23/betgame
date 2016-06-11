2ba76fe4db5504618da16b13e6ecda2b4c6cd71d
hansmaulwurf23 <arthur.spooner@gmail.com>
2016-06-11 18:11:43 +0200

    fixing non unique display names

--- grails-app/controllers/de/betgame/HomeController.groovy
+++ grails-app/controllers/de/betgame/HomeController.groovy
@@ -26,4 +26,30 @@ class HomeController {
 		
 		[nextGames:nextGames, lastGames:lastGames, myBets:myBets, countBets: countBets, userCount:userCount, betters:betters]
 	}
+	
+	def fixdisplay() {
+		def users = User.executeQuery("from User u where u in (select distinct b.user from Bet b)")
+		// reset display names
+		users.each { u -> 
+			u.display = u.givenname
+			u.save(flush:true)
+		}
+		
+		// fetch non unique ones
+		def multidisplay = users.countBy { it.display }.findAll { it.value > 1 }
+		def lettersFromLastName = 0
+		
+		// add surname letters until everyone has a unique display name
+		while(multidisplay) {
+			lettersFromLastName++
+			multidisplay.each { dis, num ->
+				users.findAll { it.display == dis }.each { u ->
+					u.display = "${u.givenname}${u.surname[0..(lettersFromLastName-1)]}"
+					u.save()
+				}
+			}
+			multidisplay = users.countBy { it.display }.findAll { it.value > 1 }
+		}
+		render "${users.size()}"
+	}
 }
